1e4191edd82512eee9ac5a767a0c1e5d1fb471f2
RabbitOpen <709498191@qq.com>
2020-07-10 17:51:43 +0800

    init

---
+++ libra-server/src/main/java/rabbit/open/libra/LibraEntry.java
@@ -0,0 +1,17 @@
+package rabbit.open.libra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 调度服务端入口程序
+ * @author xiaoqianbin
+ * @date 2020/7/10
+ **/
+@SpringBootApplication
+public class LibraEntry {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraEntry.class);
+    }
+}
